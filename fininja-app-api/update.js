07632a2d7ec9e69b0958c7743d95c5b3cb0b7048
umatbro <umatbroo@gmail.com>
2020-03-28 18:11:11 +0100

    Update entry

---
+++ fininja-app-api/update.js
@@ -0,0 +1,30 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            entryId: event.pathParameters.id,
+        },
+        UpdateExpression: "SET #value = :value, attachment = :attachment",
+        ExpressionAttributeValues: {
+            ":attachment": data.attachment || null,
+            ":value": data.value || null,
+        },
+        ExpressionAttributeNames: {
+            "#value": "value",
+        },
+        ReturnValues: "ALL_NEW",
+    };
+
+    try {
+        await dynamoDbLib.call("update", params);
+        return success({ status: true });
+    } catch (e) {
+        console.log("error", e);
+        return failure({ status: false });
+    }
+};
